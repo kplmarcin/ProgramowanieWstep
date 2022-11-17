54681642432e5adae106f997cc45a7ecf7684b5b
kplmarcin <w67241@student.wsiz.edu.pl>
2022-11-17 17:38:41 +0100

    Laboratorium4

--- Laboratorium4/5.py
+++ Laboratorium4/5.py
@@ -7,7 +7,7 @@
 #  Oblicz, ile osób zdobyło punkty powyżej, a ile poniżej średniej
 #  Wyświetl punkty poniżej średniej
 #  Wyświetl punkty powyżej średniej
-n = 15
+'''B n = 15
 import random
 punkty =[]
 for x in range(15):
@@ -32,7 +32,36 @@ for i in range(n):
     if punkty[i] > s:
         punkty_2.append(punkty(i))
 print(punkty_2)
-'''od Grażyna Szostek do każdy:    6:47 PM
+
 for p in punkty:
     if p > sr:
-        punkty_2.append(p)'''
+        punkty_2.append(p) KONIEC'''
+
+
+import random
+punkty = []
+liczba_stud = 15
+for i in range(liczba_stud):
+    # punkty = random.random() * 100 % 50
+    p = random.uniform(0, 50)
+    punkty.append(round(p, 2))
+print()
+print(punkty)
+print(f'min = {min(punkty)}')
+print(f'max = {max(punkty)}')
+p = float(input('Podaj liczbę punktów: '))
+if p in punkty:
+    print(punkty.index(p))
+else:
+    print(f'liczba {p} punktów nie występuje na liście')
+print()
+y = sum(punkty)
+sr = round(y/liczba_stud, 2)
+print("Średnia: ", sr)
+punkty_w = []
+punkty_n = []
+for i in range(liczba_stud):
+    if punkty[i] > sr:
+        punkty_w.append(punkty[i])
+punkty_n = [punkty for l in punkty if l < sr]
+print("Punkty powyżej średniej: ", punkty_w , len(punkty_w), "Punkty poniżej średniej: ", punkty_n, len(punkty_n))
